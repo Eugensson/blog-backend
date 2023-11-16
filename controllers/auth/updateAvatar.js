f31d5f530b61a94296bec728371c9e5ddcbfc39c
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2023-11-16 18:05:04 +0200

    Update uptateAvatar-controller and add logics saving avatars on Cloudinary

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,29 +1,20 @@
-const path = require("path");
-const fs = require("fs/promises");
-const Jimp = require("jimp");
+const cloudinary = require("cloudinary").v2;
 
 const {User} = require("../../models/user");
 const {ctrlWrapper} = require("../../helpers");
 
-const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 const updateAvatar = async (req, res) => {
     const {_id} = req.user;
-    const {path: tempUpload, originalname} = req.file;
-    const filename = `${_id}_${originalname}`;
-    const resultUpload = path.join(avatarsDir, filename);
-
-    Jimp.read(tempUpload, (err, avatar) => {
-        if (err) throw err;
-        avatar.resize(100, 100).write(tempUpload, () => {
-          fs.rename(tempUpload, resultUpload);
-          const avatarURL = path.join("avatars", filename);
-    
-          User.findByIdAndUpdate(_id, { avatarURL }).then(() => {
-            res.json({ avatarURL });
-          });
-        });
-    });
+    const {path} = req.file;
+    const result = await cloudinary.uploader.upload(path);
+    await User.findByIdAndUpdate(_id, { avatarURL: result.secure_url });
+    res.json({avatarURL: `${result.secure_url}`})
 }
 
 module.exports = {
